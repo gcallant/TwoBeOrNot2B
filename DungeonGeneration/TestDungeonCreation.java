39b03ae6928ff5cf090c8ca71aea43f62e05b949
Michael McConnell <Michael McConnell>
2016-04-07 19:40:13 -0700

    Removing old files

--- DungeonGeneration/TestDungeonCreation.java
+++
@@ -1,9 +0,0 @@
-public class TestDungeonCreation
-{
-   public static void main(String[] args)
-   {
-      GenerateDungeon dungeon = new GenerateDungeon(10);
-      dungeon.generatePath();
-      System.out.println(dungeon);
-   }
-}

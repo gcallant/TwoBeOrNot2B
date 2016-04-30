b9667fc31310002ac2c76e237f32c2f1f6676f9d
Breeanna Page <breeannapage@eagles.ewu.edu>
2016-04-29 18:56:08 -0700

    I am committing the changes I have made to the A_Character class. It's not entirely complete, but you can now give them a name, health, strength, speed, etc, and it will create the characters for you.

---
+++ AttackAndDefendBehavior/AttackBehaviorA.java
@@ -0,0 +1,16 @@
+package AttackAndDefendBehavior;
+
+/**
+ * Created by SaraPage on 4/29/2016.
+ */
+public class AttackBehaviorA implements I_Attack
+{
+	public AttackBehaviorA()
+	{
+	}
+
+	public void performAttack()
+	{
+		System.out.println("The character performs Attack A");
+	}
+}
--- Item/Storable.java
+++
@@ -1,7 +0,0 @@
-package Item;
-
-public interface Storable 
-{
-   public void use(Character.Character character);
-   public int getPower();
-}

6adf2e32599372fd39d55db98986db45b6a7f5ef
Michael McConnell <mmcconnell@eagles.ewu.edu>
2016-05-02 14:08:53 -0700

    Small Edits

---
+++ Item/DamageType.java
@@ -0,0 +1,17 @@
+package Item;
+
+public enum DamageType
+{
+   Piercing, Bludgeoning, Slashing;
+   
+   public String toString()
+   {
+      switch(this)
+      {
+         case Piercing: return "Piercing";
+         case Bludgeoning: return "Bludgeoning";
+         case Slashing: return "Slashing";
+         default: return "N/A";
+      }
+   }
+}

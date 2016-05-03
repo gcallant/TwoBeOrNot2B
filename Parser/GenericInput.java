651d7a73ef6f9f131aef81a8dcebb70d748ced81
Grant Callant <grant@grantcallant.com>
2016-05-02 22:04:17 -0700

    Created a new Generic Input type

---
+++ Parser/GenericInput.java
@@ -0,0 +1,16 @@
+package Parser;
+
+import java.io.Reader;
+
+/**
+ * Created by Grant Callant on 4/29/16. TwoBeOrNot2B
+ */
+public class GenericInput
+{
+	private Reader input = null;
+
+	GenericInput()
+	{
+
+	}
+}

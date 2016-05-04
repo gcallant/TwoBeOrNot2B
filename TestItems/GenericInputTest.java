304338e3532096135383dd60a46c8ebe2c738845
Grant Callant <grant@grantcallant.com>
2016-05-04 13:48:00 -0700

    Created test Suite for GenericInput

---
+++ TestItems/GenericInputTest.java
@@ -0,0 +1,22 @@
+package TestItems;
+
+import Parser.GenericInput;
+
+/**
+ * Created by Grant Callant on 5/4/16. GitHub
+ */
+public class GenericInputTest
+{
+	@org.junit.Before
+	public void setUp() throws Exception
+	{
+		GenericInput genericInput;
+	}
+
+	@org.junit.After
+	public void tearDown() throws Exception
+	{
+
+	}
+	
+}

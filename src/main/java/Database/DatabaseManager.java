568e907f2319f949a1d2e18c1b59b39b8136c842
Grant Callant II <grant@grantcallant.com>
2016-05-12 17:48:23 -0700

    Added DatabaseManager class to manage database read and write.

---
+++ src/main/java/Database/DatabaseManager.java
@@ -0,0 +1,43 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Grant Callant on 5/12/2016. Manages an SQLlite database object called database- constructor will create
+ * database if it does not exist, or connect to existing database.
+ *
+ * @author Grant Callant
+ */
+public class DatabaseManager
+{
+	private static final String     DATABASE          = "jdbc:sqlite:database";
+	private              Connection databaseConnector = null;
+	private              Statement  sqlStatement      = null;
+
+	public DatabaseManager()
+	{
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+			databaseConnector = DriverManager.getConnection(DATABASE);
+			createTables();
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private void createTables() throws SQLException
+	{
+		sqlStatement = databaseConnector.createStatement();
+
+	}
+}
